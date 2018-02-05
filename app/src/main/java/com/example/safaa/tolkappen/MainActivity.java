a98a2640a45aa8e403c4b41708bcf884de782323
Safa Al <safa.alkhelfa0076@stud.hkr.se>
2018-02-05 17:12:08 +0100

    First commit

---
+++ app/src/main/java/com/example/safaa/tolkappen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.safaa.tolkappen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main );
+    }
+
+    public void onButtonClick (View view ){
+
+        if(view.getId() == R.id.buttonBlankett){
+            Intent i = new Intent(MainActivity.this , StartMenu.class) ;
+            startActivity(i);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/safaa/tolkappen/StartMenu.java
@@ -0,0 +1,47 @@
+package com.example.safaa.tolkappen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+/**
+ * Created by safaa on 2018-02-02.
+ */
+
+public class StartMenu extends Activity {
+
+
+    private static Button urlButton;
+    private static WebView browser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_menu);
+        openUrl();
+    }
+
+    public void openUrl(){
+        urlButton = (Button)findViewById(R.id.button);
+        browser = (WebView)findViewById(R.id.webView);
+
+        urlButton.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        //String url = url_text.getText().toString();
+                        String url = "http://128.199.46.182/arabisk/CSN/0/0.html";
+                        browser.getSettings().setLoadsImagesAutomatically(true);
+                        browser.getSettings().setJavaScriptEnabled(true);
+                        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                        browser.loadUrl(url);
+                    }
+                }
+        );
+    }
+
+
+
+
+}
